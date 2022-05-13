b6cb24172d89e3c7f6c13389303f7e5e8a3aabc2
Hasireddy <malikireddy.uma@gmail.com>
2022-05-13 14:33:28 +0200

    homepage

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,7 +4,7 @@ export default function Home() {
       <div className="image"></div>
       <div className="row d-flex justify-content-center flex-column h-100 ">
 
-        <h1 class="text-white fs-1 fw-bold">START YOUR DAY WITH COFFEE</h1>
+        <h1 class="text-white fs-1 fw-bold">START YOUR DAY WITH COFFEE ee</h1>
         <p className="text-white fs-4 fst-italic mt-2">A cup of Coffee shared with friends is happiness tasted and time well spent</p>
         <div className="">
           <button className="btn btn-outline-light px-5 py-2 rounded fs-1 bg-dark me-2">search</button>
