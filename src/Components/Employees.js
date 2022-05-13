4f3a75c04964af79a4a7ab8115f7dd70d73122fb
Matthias Wieser <matthias.wieser.83@live.de>
2022-05-13 13:18:44 +0200

    Create component for employees card

--- src/Components/Employees.js
+++ src/Components/Employees.js
@@ -1,22 +1,33 @@
-import React, { useEffect } from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import EmployeesCard from './EmployeesCard';
 
 const Employees = () => {
+    const [people, setPeople] = useState();
+
     useEffect(() => {
-        
+        axios
+            .get('/TempEmployees.json')
+            .then(res => {
+                // console.log(res.data.results);
+                setPeople(res.data.results);
+            })
+            .catch();
     }, [])
 
     return (
         <>
-            <div className={`container`}>
-                <div className={`card`}>
-                    <div className={`cardHeader`}>
-                        <div>Picture</div>
-                    </div>
-                    <div className={`cardBody`}>
-                        <div>Details</div>
-                    </div>
-                </div>
-            </div>
+            <h2>Our Staff</h2>
+            <h3>to ensure your pleasant stay</h3>
+            {
+                (people) ? (
+                    people.map((person, index) => {
+                        return <EmployeesCard key={index} person={person} />
+                    })
+                ) : (
+                    'Loading...'
+                )
+            }
         </>
     )
 }
---
+++ src/Components/EmployeesCard.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const EmployeesCard = ({ person }) => {
+    return (
+        <div className={`container my-3`}>
+            <div className={`card`} style={
+                {
+                    width: '18rem',
+                    backgroundColor: '#f9f9f9',
+                    boxShadow: '0 0 10px #ddd'
+                }
+            }>
+                <div className={`cardHeader`}>
+                    {/* <p>{people}</p> */}
+                    <img
+                        src={`${person.picture.large}`}
+                        alt={`${person.name.first} ${person.name.last}`}
+                        style={
+                            {
+                                width: '100%',
+                                height: 'auto'
+                            }
+                        }
+                    />
+                </div>
+                <div className={`cardBody`}>
+                    <div className='container'>
+                        <h4>Details:</h4>
+                        <p>Hi, my name is {person.name.first} and I am {person.dob.age} years old.</p>
+                        <p></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default EmployeesCard
